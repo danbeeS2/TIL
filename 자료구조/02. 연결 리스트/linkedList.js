95728074e9da0165f0af1613401a8f3c0de2d47a
박단비 <silverwill39@gmail.com>
2025-07-12 01:05:41 +0900

    TIL: 2025.07.11 연결리스트 remove 구현

--- 자료구조/02. 연결 리스트/linkedList.js
+++ 자료구조/02. 연결 리스트/linkedList.js
@@ -22,6 +22,75 @@ class LinkdeList {
     return this.length;
   }
 
+  // 조회
+  searchByIndex(index) {
+    const current = this.#search(index)[1];
+    return current ? current.value : null;
+  }
+
+  searchByValue(value) {
+    const current = this.#searchByValue(value)[1];
+    return current ? current.value : null;
+  }
+
+  // 삭제
+  // prev → current → current.next
+  // prev.next == current ⇒ prev.next == current.next
+  removeByIndex(index) {
+    const [prev, current] = this.#search(index);
+    if (!current) return null; // 삭제하고자 하는 대상이 없을 때
+
+    if (prev) {
+      prev.next = current.next;
+    } else {
+      this.head = current.next; // index가 0일 때
+    }
+
+    this.length--;
+    return this.length;
+  }
+
+  removeByValue(value) {
+    const [prev, current] = this.#searchByValue(value);
+
+    if (!current) return false;
+    if (prev) {
+      prev.next = current.next;
+    } else {
+      this.head = current.next;
+    }
+
+    this.length--;
+    return true;
+  }
+
+  // private 메서드: 인덱스로 노드 찾기
+  #search(index) {
+    if (index < 0 || index >= this.length) return [null, null];
+
+    let prev;
+    let current = this.head;
+
+    for (let i = 0; i < index; i++) {
+      prev = current;
+      current = current.next;
+    }
+    return [prev, current];
+  }
+
+  // private 메서드: 값으로 노드 찾기
+  #searchByValue(value) {
+    let prev = null;
+    let current = this.head;
+
+    while (current) {
+      if (current.value === value) return [prev, current];
+      prev = current;
+      current = current.next;
+    }
+    return [null, null];
+  }
+
   // 연결 리스트 전체 출력
   print() {
     let result = "";
@@ -34,25 +103,6 @@ class LinkdeList {
 
     result += "null";
     console.log(result);
-  }
-
-  // 조회
-  searchByIndex(index) {
-    if (index < 0 || index >= this.length) return null;
-    let currnet = this.head;
-    for (let i = 0; i < index; i++) {
-      currnet = currnet.next;
-    }
-    return currnet.value;
-  }
-
-  searchByValue(value) {
-    let current = this.head;
-    while (current) {
-      if (current.value === value) return current.value;
-      current = current.next;
-    }
-    return null;
   }
 }
 
@@ -67,23 +117,31 @@ class Node {
 const ll = new LinkdeList();
 ll.length;
 
-ll.add(1); // 1
-ll.add(2); // 2
-ll.add(3); // 3
-ll.print() // [1] → [2] → [3] → null
-ll.add(4); // 4
-ll.add(5); // 5
+ll.add(1);
+ll.add(2);
+ll.add(3);
+ll.print(); // [1] → [2] → [3] → null
+ll.add(4);
+ll.add(5);
 console.log(ll.add(6)); // 6
 ll.print(); // [1] → [2] → [3] → [4] → [5] → [6] → null
 
-console.log(ll.searchByIndex(0)); // 1
-console.log(ll.searchByIndex(2)); // 3
-console.log(ll.searchByIndex(5)); // 6
-console.log(ll.searchByIndex(6)); // null
-console.log(ll.searchByIndex(-1)); // null
+ll.searchByIndex(0); // 1
+ll.searchByIndex(2); // 3
+ll.searchByIndex(5); // 6
+ll.searchByIndex(6); // null
+ll.searchByIndex(-1); // null
 
-console.log(ll.searchByValue(1)); // 1
-console.log(ll.searchByValue(2)); // 2
-console.log(ll.searchByValue(3)); // 3
-console.log(ll.searchByValue(6)); // 6
-console.log(ll.searchByValue(-1)); // null
+ll.searchByValue(1);
+ll.searchByValue(2);
+ll.searchByValue(3);
+ll.searchByValue(6);
+ll.searchByValue(-1); // null
+
+ll.print(); // [1] → [2] → [3] → [4] → [5] → [6] → null
+ll.searchByIndex(5); // 6
+console.log(ll.removeByIndex(5)); // 5
+ll.print(); // [1] → [2] → [3] → [4] → [5] → null
+console.log(ll.removeByValue(1)); // true
+ll.print(); // [2] → [3] → [4] → [5] → null
+ll.searchByIndex(5); // null
