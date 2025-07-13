1a302f0e98489bff4ed7f4ebf4b9d9ba3adf4a8f
박단비 <silverwill39@gmail.com>
2025-07-13 20:21:14 +0900

    TIL: 2025.07.13 LinkedList 기반 Stack 및 Queue 클래스 구현

--- 자료구조/03.큐, 스택/queue.js
+++ 자료구조/03.큐, 스택/queue.js
@@ -34,3 +34,70 @@ queue.dequeue(); // 3
 queue.dequeue(); // 5
 
 console.log(queue.peek()); // 2
+
+
+// 숙제 2. Linked list로 queue 구현하기
+export class LinkedListQueue {
+  head = null;
+  tail = null;
+  size = 0;
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+    return this.size;
+  }
+
+  dequeue() {
+    if (!this.head) return null;
+    const val = this.head.value;
+    this.head = this.head.next;
+    this.size--;
+
+    // 마지막 요소 제거 시
+    if (this.head === null) this.tail = null;
+
+    return val;
+  }
+
+  peek() {
+    return this.head?.value || null;
+  }
+
+  get lenght() {
+    return this.size;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+const llQueue = new LinkedListQueue();
+llQueue.enqueue(1);
+llQueue.enqueue(2);
+llQueue.enqueue(3);
+llQueue.enqueue(4);
+llQueue.enqueue(5);
+
+console.log(llQueue.peek()); // 1
+
+llQueue.dequeue(); // 1 제거
+llQueue.dequeue(); // 2 제거
+llQueue.dequeue(); // 3 제거
+console.log(llQueue.lenght); // 2
+
+llQueue.dequeue(); // 4 제거
+console.log(llQueue.peek()); // 5
+llQueue.dequeue(); // 5 제거
+llQueue.dequeue(); // null
--- 자료구조/03.큐, 스택/stack.js
+++ 자료구조/03.큐, 스택/stack.js
@@ -18,19 +18,77 @@ export class Stack {
   }
 }
 
-const stack = new Stack()
-stack.push(6)
-stack.push(3)
-stack.push(5)
-stack.push(2)
-stack.push(4)
-stack.push(1)
+const stack = new Stack();
+stack.push(6);
+stack.push(3);
+stack.push(5);
+stack.push(2);
+stack.push(4);
+stack.push(1);
 
-console.log(stack.top()) // 1
-console.log(stack.lenght) // 6
+console.log(stack.top()); // 1
+console.log(stack.lenght); // 6
 
-stack.pop() // 1
-stack.pop() // 4
-stack.pop() // 2
+stack.pop(); // 1
+stack.pop(); // 4
+stack.pop(); // 2
 
-console.log(stack.top()) // 5
+console.log(stack.top()); // 5
+
+// 숙제 1. Linked list로 stack 구현하기
+export class LinkedListStack {
+  top = null;
+  size = 0;
+
+  push(value) {
+    const newNode = new Node(value);
+    newNode.next = this.top;
+    this.top = newNode;
+    this.size++;
+
+    return this.size;
+  }
+
+  pop() {
+    if (!this.top) return null;
+
+    const val = this.top.value;
+    this.top = this.top.next;
+    this.size--;
+    return val;
+  }
+
+  peek() {
+    return this.top?.value || null;
+  }
+
+  get length() {
+    return this.size;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+const llStack = new LinkedListStack();
+llStack.push(1);
+llStack.push(3);
+llStack.push(2);
+llStack.push(5);
+llStack.push(4);
+
+console.log(llStack.peek()); // 4
+
+llStack.pop(); // 4 제거
+llStack.pop(); // 5 제거
+llStack.pop(); // 2 제거
+
+console.log(llStack.length); // 2
+console.log(llStack.peek()); // 3
+
+llStack.push(6);
+console.log(llStack.peek()); // 6
