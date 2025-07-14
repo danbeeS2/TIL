faedefb1278d155aaca92c992fb428c79f7aab78
박단비 <silverwill39@gmail.com>
2025-07-14 17:30:12 +0900

    TIL: 2025.07.14 Binary Search Tree - search 구현

--- 자료구조/04. 이진 탐색 트리/binarySearchTree.js
+++ 자료구조/04. 이진 탐색 트리/binarySearchTree.js
@@ -2,6 +2,9 @@ export class BinarySearchTree {
   root = null;
 
   insert(value) {
+    // 어떤 값을 넣으려할때, 일단 어디에 넣을지 모르겠다.
+    // 그래서 왼팔, 오른팔한테 맡긴다.
+    // 근데 만약 왼팔 오른팔이 없으면 거기다가 넣는다.
     this.root = this.#insertNode(this.root, value);
   }
 
@@ -26,6 +29,22 @@ export class BinarySearchTree {
 
     return node;
   }
+
+  search(value) {
+    // 어떤 값을 찾으려할때, 일단 어디에 있는지 모르겠다.
+    // 그래서 왼팔, 오른팔한테 맡긴다.
+    // 찾으면 그 노드 return, 못찾으면 null return
+    return this.#search(this.root, value);
+  }
+
+  #search(node, value) {
+    if (!node) return null; // 찾는 값이 없으면 null 리턴
+    if (node.value === value) return node; // 같은 값을 찾으면 리턴
+    // 더 작은 값이면 왼쪽, 더 큰 값이면 오른쪽
+    return node.value > value
+      ? this.#search(node.left, value)
+      : this.#search(node.right, value);
+  }
 }
 
 class Node {
@@ -40,3 +59,6 @@ const bst = new BinarySearchTree();
 [8, 10, 3, 1, 14, 6, 7, 4, 13].forEach((num) => bst.insert(num));
 
 console.log(JSON.stringify(bst.root, null, 2));
+console.log(bst.search(7)); // Node { value: 7, left: null, right: null }
+console.log(bst.search(8)); // Node { value: 8, left: Node, right: Node }
+console.log(bst.search(5)); // null
